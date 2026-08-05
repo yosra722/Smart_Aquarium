a28bf6e6fa72f760e5ff5ae6b2eb02025f0ea0f7
Yosra Madkour <yosra.madkour@gmail.com>
2026-08-05 21:49:48 +0300

    Add files via upload

---
+++ Drivers/MCAL/4-ADC/ADC_private.h
@@ -0,0 +1,88 @@
+/*****************************************/
+/******** Author: Yosra Madkour **********/
+/******** Date  : 3/8/2026   *************/
+/******** File  : Private File ***********/
+/*****************************************/
+
+#ifndef ADC_PRIVATE_H
+#define ADC_PRIVATE_H
+
+typedef void (*pf) (void);
+
+/*ADMUX*/
+#define ADMUX					*((volatile u8*)0x27)
+#define REFS1					7
+#define REFS0					6
+#define ADLAR 					5
+#define ADC_C4					4
+#define ADC_C3					3
+#define ADC_C2					2
+#define ADC_C1					1
+#define ADC_C0					0
+
+
+
+/*channels*/
+#define ADC0    				0
+#define ADC1    				1
+#define ADC2    				2
+#define ADC3    				3
+#define ADC4    				4
+#define ADC5    				5
+#define ADC6    				6
+#define ADC7    				7
+
+/*ADLAR*/
+#define LEFT_ADJUSTMEN 			1
+#define RIGHT_ADJUSTMEN 		0
+
+
+/*ADCSRA*/
+#define ADCSRA					*((volatile u8*)0x26)
+#define ADEN					7
+#define ADSC					6
+#define ADATE 					5
+#define ADIF					4
+#define ADIE					3
+#define ADPS2					2
+#define ADPS1					1
+#define ADPS0					0
+
+
+/*SFIOR*/
+#define SFIOR					*((volatile u8*)0x50)
+#define ADTS2					7
+#define ADTS1					6
+#define ADTS0					5
+#define ACME					3
+#define PUD						2
+#define PSR2					1
+#define PSR10					0
+
+
+/*ADC*/
+#define ADCL					*((volatile u8*)0x24)
+#define ADCH					*((volatile u8*)0x25)
+
+
+
+/*reference*/
+#define AREF					1
+#define AVCC					2
+#define Internal_REF 			3
+
+/*AUTO_TIGGER_MODES*/
+#define FREE_RUNNING			0
+#define ANAOLG_COM				1
+#define EX_INT					2
+#define TIMER0_COM_MATCH		3
+#define TIMER0_OVERFLOW			4
+#define TIMER1_COM_MATCH		5
+#define TIMER1_OVERFLOW			6
+#define TIMER1_CAP_EVENT		7
+
+
+
+#endif
+
+//ayatabs
---
+++ Drivers/MCAL/4-ADC/ADC_prog.c
@@ -0,0 +1,153 @@
+/*****************************************/
+/******** Author: Yosra Madkour **********/
+/******** Date  : 3/8/2026   *************/
+/******** File  : Program File ***********/
+/*****************************************/
+
+#include "Std_Types.h"
+#include "Bit_Math.h"
+
+
+
+#include "DIO_int.h"
+#include "ADC_config.h"
+#include "ADC_int.h"
+#include "ADC_private.h"
+
+
+
+
+
+
+void ADC_voidInit(void)
+{
+	ADC_voidADCDisable();
+	CLR_BIT(ADCSRA,ADSC);
+	CLR_BIT(ADCSRA,ADATE);
+	CLR_BIT(ADCSRA,ADIE);
+	SET_BIT(ADCSRA,ADIF);
+
+	#if REG_ADJ == RIGHT_ADJUSTMEN
+	CLR_BIT(ADMUX,ADLAR);
+	#elif REG_ADJ == LEFT_ADJUSTMEN
+	SET_BIT(ADMUX,ADLAR);
+	#endif
+
+	ADC_voidSelectChannel(ADC_CHANNEL);
+	ADC_voidSelectClock();
+	ADC_voidSelectRef(REF_SOURCE);
+
+}
+
+
+
+
+void ADC_voidADCEnable(void)
+{
+	SET_BIT(ADCSRA,ADEN);
+}
+
+
+void ADC_voidADCDisable(void){
+	CLR_BIT(ADCSRA,ADEN);
+}
+
+void ADC_voidSelectChannel(u8 u8channel)
+{
+	ADMUX &= 0xE0;
+	ADMUX |= u8channel;
+
+
+}
+
+void ADC_voidSelectRef(u8 u8ref)
+{
+	switch(u8ref){
+	case AREF:
+		CLR_BIT(ADMUX,REFS0);
+		CLR_BIT(ADMUX,REFS1);
+		break;
+	case AVCC:
+		SET_BIT(ADMUX,REFS0);
+		CLR_BIT(ADMUX,REFS1);
+		break;
+	case Internal_REF:
+		SET_BIT(ADMUX,REFS0);
+		SET_BIT(ADMUX,REFS1);
+		break;
+	}
+
+}
+
+void ADC_voidSelectClock(void)
+{
+    SET_BIT(ADCSRA, ADPS2);
+    SET_BIT(ADCSRA, ADPS1);
+    SET_BIT(ADCSRA, ADPS0);
+}
+
+u16 ADC_u16Read(void)
+{
+	u16 value ;
+
+	while(!GET_BIT(ADCSRA, ADIF));
+	SET_BIT(ADCSRA, ADIF);
+
+	u16 low_reg=ADCL;
+	u16 high_reg = ADCH;
+
+	#if REG_ADJ == LEFT_ADJUSTMEN
+		value = (low_reg>>6)|(high_reg<<2);
+	#elif REG_ADJ == RIGHT_ADJUSTMEN
+		value = low_reg | (high_reg<<8);
+	#endif
+
+	return value;
+}
+
+
+
+
+void ADC_voidStartConverstion()
+{
+	SET_BIT(ADCSRA,ADSC);
+}
+
+
+
+void ADC_voidAutoTrigger(void)
+{
+
+	SET_BIT(ADCSRA,ADATE);
+	SFIOR &=0x1F;
+	SFIOR |=(FREE_RUNNING<<5);
+
+
+}
+
+
+
+
+void ADC_voidInterruptEnable(void)
+{
+	SET_BIT(ADCSRA,ADIE);
+}
+void ADC_voidInterruptDisable(void)
+{
+	CLR_BIT(ADCSRA,ADIE);
+}
+
+pf ADC_CallBack;
+
+void ADC_voidSetCallBack(pf FunctionAddress)
+{
+	ADC_CallBack = FunctionAddress;
+}
+
+
+void __vector_20(void)  __attribute__((signal, used));
+void __vector_20(void)
+{
+	ADC_CallBack();
+}
+
