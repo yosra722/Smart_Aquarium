bec5e4e1495c8c658ecbf8c7e7cc20903eab7b69
khadijaNaji-hue <khadeja.mn005@gmail.com>
2026-08-06 01:58:16 +0300

    Update STOP_WATCH_int.h

--- Drivers/APP/STOP_WATCH/STOP_WATCH_int.h
+++ Drivers/APP/STOP_WATCH/STOP_WATCH_int.h
@@ -7,9 +7,10 @@
 
 #define STOP_WATCH_INT_H
 #ifdef  STOP_WATCH_INT_H
+//depend on TIM0
 void STW_voidInitialization(void) ;
 void STW_voidStart(void);        //start count
-u16  STW_u16StopANDWatch(void);  // Stop counting and init STW
-u16  STW_u16Watch(void);         //counting will continue
+u16  STW_u16StopANDWatch_usec(void);  // Stop counting and init STW
+u16  STW_u16Watch__usec(void);         //counting will continue _usec== microsecond
 
 #endif
