3479a08ca957f7dde5c5711e4a3cee13328a6bc2
Yosra Madkour <yosra.madkour@gmail.com>
2026-08-06 10:05:57 +0300

    Update LCD_config.h

--- Drivers/HAL/2- LCD/LCD_config.h
+++ Drivers/HAL/2- LCD/LCD_config.h
@@ -9,20 +9,23 @@
 #ifdef  LCD_CONFIG_H
 
 
-#define LCD_RS   PORTBID,PIN0
-#define LCD_RW   PORTBID,PIN1
-#define LCD_EN   PORTBID,PIN2
+#define LCD_RS   PORTAID,PIN1
+#define LCD_RW   PORTAID,PIN2
+#define LCD_EN   PORTAID,PIN3
 
 
- 
-#define LCD_D0 PORTAID,PIN0
-#define LCD_D1 PORTAID,PIN1
-#define LCD_D2 PORTAID,PIN2
-#define LCD_D3 PORTAID,PIN3
-#define LCD_D4 PORTAID,PIN4
-#define LCD_D5 PORTAID,PIN5
-#define LCD_D6 PORTAID,PIN6
-#define LCD_D7 PORTAID,PIN7
 
+#define LCD_D0 PORTBID,PIN0
+#define LCD_D1 PORTBID,PIN1
+#define LCD_D2 PORTBID,PIN2
+#define LCD_D3 PORTBID,PIN3
+#define LCD_D4 PORTBID,PIN4
+#define LCD_D5 PORTBID,PIN5
+#define LCD_D6 PORTBID,PIN6
+#define LCD_D7 PORTBID,PIN7
+
+#define SET_UP				LCD_FunctionSet_8b_2l_57
+#define DISPLAY_SETUP		LCD_Display_ON_Cursor_OFF
+#define ENTER_MODE			LCD_Enter_Mode_INC
 
 #endif
