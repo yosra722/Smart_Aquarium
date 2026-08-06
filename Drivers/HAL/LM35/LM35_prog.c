aaa38bbe664bb651a6952f8c0b78c2bb798f39a2
Yosra Madkour <yosra.madkour@gmail.com>
2026-08-06 13:57:43 +0300

    Update LM35_prog.c

--- Drivers/HAL/LM35/LM35_prog.c
+++ Drivers/HAL/LM35/LM35_prog.c
@@ -7,8 +7,6 @@
 #include "Std_Types.h"
 #include "Bit_Math.h"
 
-#include <avr/delay.h>
-
 #include"DIO_int.h"
 #include"ADC_int.h"
 
@@ -17,37 +15,27 @@
 #include"LM35_config.h"
 
 
+
 void LM35_voidInit(void)
 {
 	DIO_voidSetPinDirection(LM35_VOUT,INPUT);
+
 	ADC_voidInit();
+
 	ADC_voidADCEnable();
-	ADC_voidInterruptEnable();
+
 	LCD_GotoXY(0,0);
 	LCD_voidWriteMoveString("TEMP : ");
 }
 u16 LM35_u16GetTemp(void)
 {
-
 	ADC_voidStartConverstion();
-	u16 value = ADC_u16Read();
-    value = (value*500)/1024;
-    if(value <= 20 && value >=0)
-    {
-    	ADC_voidInterruptEnable();
-    	ADC_voidSetCallBack(LM35_voidINT());
-    }
-    return value;
+	u16 TEMP=ADC_u16Read();
+    TEMP = (TEMP*500)/1024;
+
+    return TEMP;
 }
 
-void LM35_voidINT(void)
-{
-
-	DIO_voidSetPinDirection(PORTCID,PIN0,OUTPUT );
-	DIO_voidSetPinValue(PORTCID,PIN0,HIGH);
-	_delay_ms(100);
-	DIO_voidSetPinValue(PORTCID,PIN0,LOW);
-}
 void LM35_DisplayTemp(void)
 {
 	LM35_voidClearTem();
