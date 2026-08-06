ac793fc0f8001981fdb627d05afb9b2b9ac45609
Yosra Madkour <yosra.madkour@gmail.com>
2026-08-06 09:10:45 +0300

    Add files via upload

---
+++ Drivers/HAL/LM35/LM35_config.h
@@ -0,0 +1,13 @@
+/*****************************************/
+/******** Author: Yosra Madkour **********/
+/******** Date  : 6/8/2026   *************/
+/******** File  : Configuration File *****/
+/*****************************************/
+
+#ifndef LM35_CONFIGRATION_H
+#define LM35_CONFIGRATION_H
+
+#define  LM35_VOUT            PORT_A,PIN0
+
+
+#endif
---
+++ Drivers/HAL/LM35/LM35_prog.c
@@ -0,0 +1,64 @@
+/*****************************************/
+/******** Author: Yosra Madkour **********/
+/******** Date  : 6/8/2026   *************/
+/******** File  : Program File ***********/
+/*****************************************/
+
+#include "Std_Types.h"
+#include "Bit_Math.h"
+
+#include <avr/delay.h>
+
+#include"DIO_int.h"
+#include"ADC_int.h"
+
+#include"LM35_int.h"
+#include"LM35_private.h"
+#include"LM35_config.h"
+
+
+void LM35_voidInit(void)
+{
+	DIO_voidSetPinDirection(LM35_VOUT,INPUT);
+	ADC_voidInit();
+	ADC_voidADCEnable();
+	ADC_voidInterruptEnable();
+	LCD_GotoXY(0,0);
+	LCD_voidWriteString("TEMP : ");
+}
+u16 LM35_u16GetTemp(void)
+{
+
+	ADC_voidStartConverstion();
+	u16 value = ADC_u16Read();
+    value = (value*500)/1024;
+    if(value <= 20 && value >=0)
+    {
+    	ADC_voidInterruptEnable();
+    	ADC_voidSetCallBack(LM35_voidINT());
+    }
+    return value;
+}
+
+void LM35_voidINT(void)
+{
+
+	DIO_voidSetPinDirection(PORT_C,PIN0,OUTPUT );
+	DIO_voidSetPinValue(PORT_C,PIN0,HIGH);
+	_delay_ms(100);
+	DIO_voidSetPinValue(PORT_C,PIN0,LOW);
+}
+void LM35_DisplayTemp(void)
+{
+	LM35_voidClearTem();
+	LCD_GotoXY(7,0);
+	LCD_voidWriteNumber(LM35_u16GetTemp());
+	LCD_voidWriteString(" C");
+}
+
+
+static void LM35_voidClearTem(void)
+{
+	LCD_GotoXY(14,0);
+	LCD_voidWriteString("   ");
+}
