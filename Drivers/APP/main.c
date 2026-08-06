6d62e31412f58a747835d55a8c87a1d61084997d
khadijaNaji-hue <khadeja.mn005@gmail.com>
2026-08-06 12:01:26 +0300

    Add files via upload

---
+++ Drivers/APP/main.c
@@ -0,0 +1,82 @@
+#include "STD_Types.h"
+#include "Bit_Math.h"
+
+#include "DIO_int.h"
+
+#include "GIE_int.h"
+#include "TIM0_int.h"
+#include "STOP_WATCH_int.h"
+#include "LCD_int.h"
+#include <util/delay.h>
+
+
+void LCD_voidPrintNumber(u32 Copy_u32Number);
+
+int main(void)
+{
+
+    DIO_voidSetPortDirection(PORTAID, OUTPUT);
+    DIO_voidSetPortValue(PORTAID, 0x00);
+
+    DIO_VoidSetPinDirection(PORTBID, PIN0, OUTPUT); // RS
+    DIO_VoidSetPinDirection(PORTBID, PIN1, OUTPUT); // RW
+    DIO_VoidSetPinDirection(PORTBID, PIN2, OUTPUT); // E
+
+
+    TIM0_voidInitialization();
+    GIE_voidEnable( );
+
+    LCD_voidIntialization();
+    STW_voidInitialization();
+
+    LCD_voidWriteString((u8*)" STW TEST START", 0);
+
+
+    while (1)
+    {
+        LCD_voidSendCommand(0x01); // Clear Display
+
+
+        LCD_voidSendCommand(0x01);
+        _delay_ms(2);
+
+
+
+        STW_voidStart();
+        _delay_us(5);                              // 2000us = 2ms
+        u16 Local_u16MeasuredUs = STW_u16StopANDWatch_usec();
+
+        LCD_voidWriteString((u8*)"Meas:", 1);
+        LCD_voidPrintNumber((u32)Local_u16MeasuredUs);
+        LCD_voidSendData('u');
+        LCD_voidSendData('s');
+
+        _delay_ms(3000);
+    }
+
+    return 0;
+}
+void LCD_voidPrintNumber(u32 Copy_u32Number)
+{
+    u8 Local_u8Buffer[100];
+    s8 Local_s8Index = 0;
+
+    if (Copy_u32Number == 0)
+    {
+        LCD_voidSendData('0');
+        return;
+    }
+
+    while (Copy_u32Number > 0)
+    {
+        Local_u8Buffer[Local_s8Index] = (Copy_u32Number % 10) + '0';
+        Copy_u32Number /= 10;
+        Local_s8Index++;
+    }
+
+    while (Local_s8Index > 0)
+    {
+        Local_s8Index--;
+        LCD_voidSendData(Local_u8Buffer[Local_s8Index]);
+    }
+}
